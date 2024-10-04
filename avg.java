066098454ae8aa1b9e2f578b764f20b93bd8c282
Kamal-nayan123 <149611588+Kamal-nayan123@users.noreply.github.com>
2024-10-04 14:31:31 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class avg{
+  public static void main(String[] args){
+    int a[]={1,2,3,4,5,6,7,8,9};int sum=0;
+    for(int i=0;i<n;i++){
+      sum+=a[i];
+    }
+    System.out.println((int)sum/n);
+  }
+}
